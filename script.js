31f76e7d8fe84907cc55dea10ab4f17b8bc7cbf9
ana <156020885+makia014@users.noreply.github.com>
2024-03-15 12:08:21 +0200

    Create and implement addToScreen() and clearScreen()

--- script.js
+++ script.js
@@ -19,3 +19,42 @@ const operate = (a, operator, b) => {
       return divide(a, b);
   }
 };
+
+const displayVal = document.querySelector(".screen");
+
+const addToScreen = (val) => {
+  if (displayVal.textContent.length < 11) {
+    if (displayVal.textContent == 0 && val == 0) {
+      displayVal.textContent = 0;
+    } else if (displayVal.textContent == 0) {
+      displayVal.textContent = val;
+    } else {
+      displayVal.textContent += val;
+    }
+  } else {
+  }
+};
+
+addToScreen(0);
+
+const clearScreen = () => {
+  displayVal.textContent = "0";
+  operandA = "";
+  operatorSymbol = "";
+  operandB = "";
+};
+
+clearScreen();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+    if (e.target.classList.contains("operand")) {
+      addToScreen(e.target.textContent);
+    }
+    if (e.target.classList.contains("clear")) {
+      clearScreen();
+    }
+  });
+});
