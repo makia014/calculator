31345743c04f3d9440c0bef172f13f7ea2c25cc7
ana <156020885+makia014@users.noreply.github.com>
2024-03-15 10:33:38 +0200

    Create variables and operate function
    The variables for first and second operands and the operate function.

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+let operandA;
+let operatorSymbol;
+let operandB;
+
+const operate = (a, operator, b) => {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+};
